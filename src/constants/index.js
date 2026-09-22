41957472a2c91fb6d2ba60bb47e9973600e6df3a
SiraatHussein <siraathussein0@gmail.com>
2026-09-22 17:35:08 +0000

    Initial and final commit

--- src/constants/index.js
+++ src/constants/index.js
@@ -50,11 +50,7 @@ const services = [
     icon: mobile,
   },
   {
-    title: "Backend Developer",
-    icon: backend,
-  },
-  {
-    title: "Content Creator",
+    title: "UI/UX Designer",
     icon: creator,
   },
 ];
@@ -117,54 +113,46 @@ const technologies = [
 const experiences = [
   {
     title: "React.js Developer",
-    company_name: "Starbucks",
     icon: starbucks,
     iconBg: "#383E56",
-    date: "March 2020 - April 2021",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Built and maintained scalable web applications using React.js, Redux, and modern JavaScript.",
+      "Translated design mockups into pixel-perfect, reusable UI components with a focus on performance.",
+      "Optimized application load times and rendering efficiency through code-splitting and lazy loading.",
+      "Worked closely with backend teams to integrate RESTful APIs and manage application state.",
     ],
   },
   {
     title: "React Native Developer",
-    company_name: "Tesla",
     icon: tesla,
     iconBg: "#E6DEDD",
-    date: "Jan 2021 - Feb 2022",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Developed cross-platform mobile applications for iOS and Android using React Native.",
+      "Implemented smooth navigation flows and native device features such as camera, push notifications, and geolocation.",
+      "Debugged and resolved platform-specific issues to ensure consistent performance across devices.",
+      "Collaborated with teams to translate business requirements into functional mobile features.",
     ],
   },
   {
     title: "Web Developer",
-    company_name: "Shopify",
     icon: shopify,
     iconBg: "#383E56",
-    date: "Jan 2022 - Jan 2023",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Designed and developed responsive, user-friendly websites and e-commerce storefronts.",
+      "Customized website themes to align with brand guidelines and design specifications.",
+      "Ensured consistent performance and appearance across multiple browsers and screen sizes.",
+      "Identified and resolved bugs while implementing new features to enhance site functionality."
     ],
   },
   {
-    title: "Full stack Developer",
-    company_name: "Meta",
+    title: "UI/UX Designer",
     icon: meta,
     iconBg: "#E6DEDD",
-    date: "Jan 2023 - Present",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Created wireframes, prototypes, and high-fidelity mockups for web and mobile applications.",
+      "Conducted user research and usability testing to inform design decisions.",
+      "Collaborated with developers to ensure accurate and consistent implementation of designs.",
+      "Established and maintained design systems to ensure visual consistency across products.",
     ],
   },
 ];
@@ -173,45 +161,39 @@ const testimonials = [
   {
     testimonial:
       "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-    name: "Sara Lee",
-    designation: "CFO",
-    company: "Acme Co",
+    name: "Bene",
     image: "https://randomuser.me/api/portraits/women/4.jpg",
   },
   {
     testimonial:
       "I've never met a web developer who truly cares about their clients' success like Rick does.",
-    name: "Chris Brown",
-    designation: "COO",
-    company: "DEF Corp",
+    name: "Kelveena",
     image: "https://randomuser.me/api/portraits/men/5.jpg",
   },
   {
     testimonial:
       "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-    name: "Lisa Wang",
-    designation: "CTO",
-    company: "456 Enterprises",
+    name: "McCarthy",
     image: "https://randomuser.me/api/portraits/women/6.jpg",
   },
 ];
 
 const projects = [
   {
-    name: "Car Rent",
+    name: "AI-powered architectural visualization website",
     description:
-      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+      "Developed a full-stack AI-powered architectural visualization web application that transforms uploaded 2D floor plans into photorealistic 3D interior designs. Built using React, TypeScript, and Puter.js. ",
     tags: [
       {
         name: "react",
         color: "blue-text-gradient",
       },
       {
-        name: "mongodb",
+        name: "puterjs",
         color: "green-text-gradient",
       },
       {
-        name: "tailwind",
+        name: "typescript",
         color: "pink-text-gradient",
       },
     ],
@@ -219,20 +201,20 @@ const projects = [
     source_code_link: "https://github.com/",
   },
   {
-    name: "Job IT",
+    name: "Emergency Service App",
     description:
-      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+      "Developed an Emergency Service application with my team which aims to deliver rapid medical assistance during critical situations. The app has an emergency button that shares the user's location with medical personnel, allows access to medical history to aid in treatment, and provides first aid materials to educate users on right steps during an emergency.",
     tags: [
       {
-        name: "react",
+        name: "react-native",
         color: "blue-text-gradient",
       },
       {
-        name: "restapi",
+        name: "typescript",
         color: "green-text-gradient",
       },
       {
-        name: "scss",
+        name: "tailwind",
         color: "pink-text-gradient",
       },
     ],
@@ -240,26 +222,71 @@ const projects = [
     source_code_link: "https://github.com/",
   },
   {
-    name: "Trip Guide",
+    name: "Menu and Ordering App",
     description:
-      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+      "Designed wireframes and interactive prototypes for a menu and ordering application built around inclusivity and accessibility-first design. The app was designed to accommodate users with visual, motor, and cognitive differences, ensuring no one is excluded from a smooth ordering experience. ",
     tags: [
       {
-        name: "nextjs",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "supabase",
-        color: "green-text-gradient",
-      },
-      {
-        name: "css",
+        name: "figma",
         color: "pink-text-gradient",
       },
     ],
     image: tripguide,
     source_code_link: "https://github.com/",
   },
+   {
+    name: "Ecommerce Mobile App",
+    description:
+      "An e-commerce mobile app centered around a smart image-based search feature, allowing users to simply upload or snap a photo of a product they're looking for and instantly receive matching in-stock results. ",
+    tags: [
+        {
+        name: "react-native",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "typescript",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "nodejs",
+        color: "green-text-gradient",
+      },
+
+    ],
+    image: ecommerce,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Snake Game",
+    description:
+      "Developed a classic implementation of the timeless Snake game, built entirely in Java. The player controls a snake that moves continuously around the game board, eating food that appear at random positions.",
+    tags: [
+      {
+        name: "java",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: snake,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Waste Management Dashboard",
+    description:
+      "Designed wireframes and prototypes for a waste management dashboard aimed at helping organizations manage waste disposal more effectively while minimizing their environmental impact. The interface provides real-time data on waste generation, disposal methods, and recycling rates, giving organizations clear visibility into their overall performance.",
+    tags: [
+      {
+        name: "figma",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: dashboard,
+    source_code_link: "https://github.com/",
+  },
+  
 ];
 
 export { services, technologies, experiences, testimonials, projects };
